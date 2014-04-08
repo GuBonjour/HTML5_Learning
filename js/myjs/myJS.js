83edaa6293193f5bc72efc2bed085117c75b197d
Gu_Bonjour <gujiayex@gmail.com>
2014-04-09 02:20:46 +0800

    creatingJS
    JS testing

--- js/myjs/myJS.js
+++ js/myjs/myJS.js
@@ -1,3 +1,37 @@
-function newjs() {
+function testjs() {
 	alert("ssss");
+}
+
+function checkUserInfo() {
+	if (myIsNull($("#name_in").val()) || myIsNull($("#code_in").val())) {
+		alert("Uid or pwd could not be empty");
+		return;
+	}
+	if ($("#name_in").val().length > parseInt($("#name_in").attr("maxlength"))) {
+		alert($("#name_in").val().length + $("#name_in").attr("maxlenght"));
+	} else {
+		alert("Pass check");
+	}
+	return;
+}
+
+function clearUI(para_id) {
+	var arr_para = para_id.split(",");
+	var para;
+	for (var i = 0; i < arr_para.length; i++) {
+		para = $("#" + arr_para[i]);
+		console.log(para.val());
+		para.val("");
+	}
+
+}
+
+function clearUserInfo() {
+	console.log("yoooooh");
+	$("#name_in").val("");
+	$("#code_in").val("");
+}
+
+function myIsNull(value) {
+	return value == null || value == "" ? true : false;
 }
